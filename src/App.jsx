418a4e28528a53927ad218c4b535913369c64b45
surajgharde <gharadesuraj@gmail.com>
2025-04-05 00:07:06 +0530

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './component/Navbar'
+
+function App() {
+  
+
+  return (
+    
+ <>
+ <Navbar/>
+ <h1>Hlw Kaise ho app</h1>
+ </>
+  )
+}
+
+export default App
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+function Navbar (){
+    return (
+        <>
+         <nav>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>About </li>
+                <li>Contact</li>
+            </ul>
+         </nav>
+        </>
+    )
+}
+
+export default Navbar;
